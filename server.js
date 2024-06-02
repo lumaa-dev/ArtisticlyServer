b934b9c83059390662ccca9cde34759bae6b82cd
Lumaa <lumaa@lumaa.fr>
2024-06-02 18:25:50 +0200

    CORS

--- server.js
+++ server.js
@@ -16,9 +16,11 @@ const {
 	accessCode,
 	serverUpdater,
 } = require("./config.json");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
